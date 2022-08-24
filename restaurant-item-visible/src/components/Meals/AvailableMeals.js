12dc6f034678dae995680b980b55cf6348811d38
Afzal <afzalalam9555@gmail.com>
2022-08-24 12:09:05 +0530

    Making-Item-Visible

--- restaurant-item-visible/src/components/Meals/AvailableMeals.js
+++ restaurant-item-visible/src/components/Meals/AvailableMeals.js
@@ -1,7 +1,9 @@
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
-/*  {
+  {
     id: 'm1',
     name: 'Sushi',
     description: 'Finest fish and veggies',
@@ -24,15 +26,24 @@ const DUMMY_MEALS = [
     name: 'Green Bowl',
     description: 'Healthy...and green...',
     price: 18.99,
-  },*/
+  },
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
